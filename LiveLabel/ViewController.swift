9aaeed3bdd7a3981deb7abaeb6d65ef4394b5c39
Apple <apple@appledemacbook-pro.local>
2016-06-20 12:14:48 +0800

    Initial Commit

---
+++ LiveLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LiveLabel
+//
+//  Created by Apple on 16/6/20.
+//  Copyright © 2016年 HeheData. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
